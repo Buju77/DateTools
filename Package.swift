530878f9f057b5b5d1876fc82a32d0cd76130269
Yen-Chia Lin <buju77@gmail.com>
2023-08-11 18:02:18 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,45 @@
 // swift-tools-version:5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "DateToolsSwift",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v12),
+        .tvOS(.v12),
+        .macOS(.v10_13),
+    ],
+    products: [
+        .library(name: "DateToolsSwift", targets: ["DateToolsSwift"]),
+        .library(name: "DateToolsObjc", targets: ["DateToolsObjc"])
+    ],
+    dependencies: [
+    ],
     targets: [
-        Target(name: "DateToolsSwift")
+        .target(
+            name: "DateToolsSwift",
+            path: "DateToolsSwift/DateTools",
+            resources: [.copy("DateTools.bundle")],
+            swiftSettings: [.define("SWIFTPM_MODULE_BUNDLE")]
+        ),
+        .target(
+            name: "DateToolsObjc",
+            path: "DateTools/DateTools",
+            resources: [.copy("DateTools.bundle")],
+            publicHeadersPath: ".",
+            cSettings: [.define("SWIFTPM_MODULE_BUNDLE")]
+        ),
+        .testTarget(
+            name: "DateToolsSwiftTests",
+            dependencies: ["DateToolsSwift"],
+            path: "DateToolsSwift/Tests/PackageTests"
+        ),
+        .testTarget(
+            name: "DateToolsObjcTests",
+            dependencies: ["DateToolsObjc"],
+            path: "DateToolsSwift/Tests/PackageTestsObjc"
+        )
     ]
 )
-package.exclude = ["DateTools", "Examples", "Tests", "DateToolsSwift/Examples"]
+  
