3e291469c393a85fd46c2875160da106bdf429c3
Yen-Chia Lin <buju77@gmail.com>
2023-08-25 19:13:48 +0200

    fix exclude path in Package manifest file

--- Package.swift
+++ Package.swift
@@ -3,8 +3,24 @@ import PackageDescription
 
 let package = Package(
     name: "DateToolsSwift",
+    defaultLocalization: "en",
+    products: [
+        .library(
+            name: "DateToolsSwift",
+            targets: ["DateToolsSwift"])
+    ],
     targets: [
-        .target(name: "DateToolsSwift")
+        .target(name: "DateToolsSwift",
+                path: "DateToolsSwift/DateTools",
+                exclude: [
+                    "DateTools",
+                    "DateTools.bundle",
+                    "Examples",
+                    "Tests",
+                    "DateTools/DateTools.bundle",
+                    "DateToolsSwift/Tests",
+                    "DateToolsSwift/DateTools/DateTools.bundle",
+                    "DateToolsSwift/Examples"],
+                resources: [.copy("../Resources/DateTools.bundle")])
     ]
 )
-package.exclude = ["DateTools", "Examples", "Tests", "DateToolsSwift/Examples"]
